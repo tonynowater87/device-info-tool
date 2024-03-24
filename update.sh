f3f7107a7aceac48d751624ee76d11d86a7ac125
tonynowater <tonynowater87@gmai..com>
2024-03-24 09:18:41 +0800

    add shell script for crontab

--- update.sh
+++ update.sh
@@ -1,1 +1,13 @@
-python crawler/apple-website-crawler.py && python crawler/update-version.py
+cd /Users/tonynowater/Documents/my-project/front-end-app/flutter/mobile-os-versions
+
+/Users/tonynowater/.pyenv/versions/3.11.1/bin/python /Users/tonynowater/Documents/my-project/front-end-app/flutter/mobile-os-versions/crawler/apple-website-crawler.py && /Users/tonynowater/.pyenv/versions/3.11.1/bin/python /Users/tonynowater/Documents/my-project/front-end-app/flutter/mobile-os-versions/crawler/update-version.py
+
+git add resources/ios-os-versions.json
+git add resources/ipad-os-versions.json
+git add resources/mac-os-versions.json
+git add resources/tv-os-versions.json
+git add resources/watch-os-versions.json
+git commit -m "update versions.json"
+git push -u origin main
+
+date +"%Y-%m-%d %H:%M:%S" >> log.txt
