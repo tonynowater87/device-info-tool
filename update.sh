e793d3b9ba9230405ce24d3c0bf69da93a2eb194
tonynowater <tonynowater87@gmai..com>
2024-04-05 21:04:26 +0800

    update crontab script

--- update.sh
+++ update.sh
@@ -1,12 +1,15 @@
 cd /Users/tonynowater/Documents/my-project/front-end-app/flutter/mobile-os-versions
 
-/Users/tonynowater/.pyenv/versions/3.11.1/bin/python /Users/tonynowater/Documents/my-project/front-end-app/flutter/mobile-os-versions/crawler/apple-website-crawler.py && /Users/tonynowater/.pyenv/versions/3.11.1/bin/python /Users/tonynowater/Documents/my-project/front-end-app/flutter/mobile-os-versions/crawler/update-version.py
+/Users/tonynowater/.pyenv/versions/3.11.1/bin/python /Users/tonynowater/Documents/my-project/front-end-app/flutter/mobile-os-versions/crawler/apple-website-crawler.py && /Users/tonynowater/.pyenv/versions/3.11.1/bin/python /Users/tonynowater/Documents/my-project/front-end-app/flutter/mobile-os-versions/crawler/update-version.py && /Users/tonynowater/.pyenv/versions/3.11.1/bin/python /Users/tonynowater/Documents/my-project/front-end-app/flutter/mobile-os-versions/crawler/mobile-distribution-crawler.py
+
 
 git add resources/ios-os-versions.json
 git add resources/ipad-os-versions.json
 git add resources/mac-os-versions.json
 git add resources/tv-os-versions.json
 git add resources/watch-os-versions.json
+git add resources/android-distribution.json
+git add resources/ios-distribution.json
 git commit -m "update versions.json"
 git push -u origin main
 
